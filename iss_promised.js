50c7c2e6faea0d46f3108a0fab9d1f02eed07025
PandaBurglar <zane.bokhari@gmail.com>
2021-12-08 18:21:47 -0500

    command line app for viewing ISS times via IP using promises

---
+++ iss_promised.js
@@ -0,0 +1,33 @@
+// iss_promised.js
+const request = require('request-promise-native');
+/*
+ * Requests user's ip address from https://www.ipify.org/
+ * Input: None
+ * Returns: Promise of request for ip data, returned as JSON string
+ */
+const fetchMyIP = function() {
+  // use request to fetch IP address from JSON API
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://freegeoip.app/json/${ip}`);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const { latitude, longitude } = JSON.parse(body);
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`);
+};
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then((data) => {
+  const { response } = JSON.parse(data);
+  return response;
+  });
+};
+
+module.exports = { nextISSTimesForMyLocation };
